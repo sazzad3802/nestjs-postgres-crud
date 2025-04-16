18c4a9f66c4a8ef13f6caebd3cc5591b862cdbe3
Sazzad H Kanon <sazzadkanon@gmail.com>
2025-04-17 01:49:32 +0600

    local config changed

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,26 +1,33 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { UserModule } from './user/user.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { User } from './user/entities/user.entity';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import * as path from 'path';
 
 @Module({
   imports: [
-    TypeOrmModule.forRoot({
-      type: 'postgres',
-      host: 'localhost',
-      port: 5432,
-      password: 'postgres',
-      username: 'postgres',
-      entities: [User],
-      database: 'nestjs_db',
-      synchronize: true,
-      logging: true,
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: [
+        path.resolve(__dirname, `../.env.${process.env.NODE_ENV || 'development'}`),
+        path.resolve(__dirname, '../.env'),
+      ],
+    }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({ 
+        type: 'postgres',
+        host: configService.get<string>('DATABASE_HOST'),
+        port: configService.get<number>('DATABASE_PORT'),
+        username: configService.get<string>('DATABASE_USER'),
+        password: configService.get<string>('DATABASE_PASSWORD'),
+        database: configService.get<string>('DATABASE_NAME'),
+        autoLoadEntities: true,
+        synchronize: true,
+      }),
     }),
     UserModule,
   ],
-  controllers: [AppController],
-  providers: [AppService],
 })
 export class AppModule {}
--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,11 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as dotenv from 'dotenv';
+
+// dotenv.config({
+//   path: `.env${process.env.NODE_ENV ? `.${process.env.NODE_ENV}` : ''}`,
+// });
+
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
